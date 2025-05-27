03fdac38621c09c72db5e17736ba5338109e1efe
Naoki Yasuraoka <yasuraok@gmail.com>
2025-05-27 11:33:59 +0900

    fixup node_modules inclusion

--- esbuild.js
+++ esbuild.js
@@ -1,14 +1,13 @@
 const { build } = require('esbuild');
-const { nodeExternalsPlugin } = require('esbuild-node-externals');
 
 build({
   entryPoints: ['./src/extension.ts'],
   bundle: true,
   outfile: './out/extension.js',
   platform: 'node',
-  target: 'node16', // Adjust according to the Node.js version you are using
-  external: ['vscode'], // 'vscode' module is provided at runtime
-  plugins: [nodeExternalsPlugin()],
-  sourcemap: true, // Generate sourcemaps for debugging
-  minify: true, // Minify code for production
+  target: 'node16',
+  packages: 'bundle',        // 依存関係をバンドルに含める
+  external: ['vscode'],      // VSCodeモジュールのみ外部化
+  sourcemap: true,
+  minify: true,
 }).catch(() => process.exit(1));
